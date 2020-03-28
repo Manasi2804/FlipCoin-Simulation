ddcaf9ab1a602c1beab0732852720a6a07a14f21
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-28 14:39:24 +0530

    Added: Flip coin simulation for doublet combination

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -35,3 +35,51 @@ echo "Head Percentage:" $percentageHead
 echo "Tail percentage: `echo " scale=2; 100-$percentageHead"  | bc `"
 }
 Singlet
+
+function Doublet() {
+HH=0
+TT=0
+HT=0
+TH=0
+
+declare -A flipCoinDictionary
+flipCoinDictionary=([HH]=$HH [TT]=$TT [HT]=$HT [TH]=$TH)
+
+read -p "Enter the number of flips:" numberOfFlipsDoublet
+for((flipCount=1;flipCount<=$numberOfFlipsDoublet;flipCount++))
+do
+	case $((RANDOM%4+1)) in
+	1)
+		flipCoinDictionary[HH]=$((${flipCoinDictionary[HH]}+1))
+		echo -ne "HH"
+		;;
+	2)
+		flipCoinDictionary[TT]=$((${flipCoinDictionary[TT]}+1))
+		echo -ne "TT"
+		;;
+	3)
+		flipCoinDictionary[HT]=$((${flipCoinDictionary[HT]}+1))
+		echo -ne "HT"
+		;;
+	4)
+		flipCoinDictionary[TH]=$((${flipCoinDictionary[TH]}+1))
+		echo -ne "TH"
+		;;
+	*)
+		echo "Invalid Choice"
+		;;
+	esac
+done
+
+echo ""
+for key in "${!flipCoinDictionary[@]}"
+do
+	echo "${key}:${flipCoinDictionary[$key]}"
+done
+
+echo "HH percentage: `echo " scale=2 ; ${flipCoinDictionary[HH]}*100/$numberOfFlipsDoublet" | bc ` "
+echo "TT percentage: `echo " scale=2 ; ${flipCoinDictionary[TT]}*100/$numberOfFlipsDoublet" | bc ` "
+echo "HT percentage: `echo " scale=2 ; ${flipCoinDictionary[HT]}*100/$numberOfFlipsDoublet" | bc ` "
+echo "TH percentage: `echo " scale=2 ; ${flipCoinDictionary[TH]}*100/$numberOfFlipsDoublet" | bc ` "
+}
+Doublet
