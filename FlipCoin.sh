900c850c7eea0ce03155381b0a00099aec76d9d8
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-28 13:59:49 +0530

    Added:Flip coin simulation for singlet combination

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,9 +1,37 @@
 #!/bin/bash -x
-Heads=1
-flipNumber=$(( RANDOM%2 ))
-if [[ $flipNumber -eq 1 ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+
+function singlet(){
+H=0
+T=0
+
+declare -A singletCombinationDictionary
+singletCombinationDictionary=([H]=$H [T]=$T)
+
+read -p"Enter the number of flips:" numberOfFlips
+for((flipCount=1;flipCount<=$numberOfFlips;flipCount++))
+do
+   case $((RANDOM%2+1)) in
+      1)
+         singletCombinationDictionary[H]=$((${singletCombinationDictionary[H]}+1))
+         echo -ne "H"
+			;;
+      2)
+         singletCombinationDictionary[T]=$((${singletCombinationDictionary[T]}+1))
+         echo -ne "T"
+      	;;
+      3)
+         echo "Invalid choice"
+      	;;
+   esac
+done
+echo ""
+for key in "${!singletCombinationDictionary[@]}"
+do
+   echo "${key}:${singletCombinationDictionary[$key]}"
+done
+
+percentageHead=`echo " scale=2 ; ${singletCombinationDictionary[H]}*100/$numberOfFlips" | bc`
+echo "Head Percentage:" $percentageHead
+echo "Tail percentage: `echo " scale=2; 100-$percentageHead"  | bc `"
+}
+Singlet
