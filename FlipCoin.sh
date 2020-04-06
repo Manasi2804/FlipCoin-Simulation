e3468ccac116e60215b570402c7fc24629e14775
Manasi2804 <61973834+Manasi2804@users.noreply.github.com>
2020-04-06 20:23:22 +0530

    Update FlipCoin.sh

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -18,28 +18,32 @@ choice=0
 declare -A flipCoinDictionary
 flipCoinDictionary=([H]=$H [T]=$T [HH]=$HH [TT]=$TT [HT]=$HT [TH]=$TH [HHH]=$HHH [TTT]=$TTT
 							[HHT]=$HHT [TTH]=$TTH [THT]=$THT [HTH]=$HTH [HTT]=$HTT [THH]=$THH)
-
+# Function for singlet combination
 function getSingletCombination(){
 
 read -p "Enter the number of flips you want to perform for singlet:" numberOfFlipSinglet
 for((flipCount=1;flipCount<=$numberOfFlipSinglet;flipCount++))
 do
-   case $((RANDOM%2+1)) in
+   case $((RANDOM%2)) in
    1)
       flipCoinDictionary[H]=$((${flipCountDictionary[H]}+1))
-      echo -ne "H"
       ;;
    2)
       flipCoinDictionary[T]=$((${flipCountDictionary[T]}+1))
-      echo -ne "T"
       ;;
    esac
 done
+	#Print dictionary of singlet combination
+	for key in "${!flipCoinDictionary[@]}"
+	do
+		echo "${key} : ${flipCoinDictionary[$key]}"
+	done
 
 echo ""
-percentageHead=`echo "scale=2; ${flipCoinDictionary[H]}*100/$numberOfFlipSinglet" | bc `
+percentageHead=`echo "scale=4; (${flipCoinDictionary[H]}*100/$numberOfFlipSinglet") | bc -1 `
+percentageTail=`echo "scale=4; 100-$percentageHead" | bc -1 `
 echo "Head Percentage: " $percentageHead
-echo "Tail Percentage: `echo " scale=2; 100-$percentageHead" | bc` "
+echo "Tail Percentage: " $percentageTail
 }
 
 function getDoubletCombination(){
@@ -47,31 +51,32 @@ function getDoubletCombination(){
 read -p "Enter the number of flips you want to perform for doublet:"numberOfFlipsDoublet
 for((flipCount=1;flipCount<=$numberOfFlipsDoublet;flipCount++))
 do
-   case $((RANDOM%4+1)) in
+   case $((RANDOM%4)) in
 	1)
- 		flipCoinDictionary[HH]=$((${flipCoinDictionary[HH]}+1))
-      echo -ne "HH"
-      ;;
-   2)
-      flipCoinDictionary[TT]=$((${flipCoinDictionary[TT]}+1))
-      echo -ne "TT"
-       ;;
-   3)
-      flipCoinDictionary[HT]=$((${flipCoinDictionary[HT]}+1))
-      echo -ne "HT"
-       ;;
-   4)
-      flipCoinDictionary[TH]=$((${flipCoinDictionary[TH]}+1))
-      echo -ne "TH"
-       ;;
+		flipCoinDictionary[HH]=$((${flipCoinDictionary[HH]}+1))
+      		;;
+   	2)	
+      		flipCoinDictionary[TT]=$((${flipCoinDictionary[TT]}+1))
+       		;;
+   	3)	
+      		flipCoinDictionary[HT]=$((${flipCoinDictionary[HT]}+1))
+		;;
+   	4)
+      		flipCoinDictionary[TH]=$((${flipCoinDictionary[TH]}+1))
+      		;;
    esac
 done
-
+	#Print the dictionary for doublet combination
+	
+	for key in "${!flipCoinDictionary[@]}"
+	do
+		echo "${key} : ${flipCoinDictionary[$key]}"
+	done
 echo ""
-echo "HH percentage:`echo "scale=2; ${flipCoinDictionary[HH]}*100/$numberOfFlipsDoublet" | bc ` "
-echo "TT percentage:`echo "scale=2; ${flipCoinDictionary[TT]}*100/$numberOfFlipsDoublet" | bc ` "
-echo "HT percentage:`echo "scale=2; ${flipCoinDictionary[HT]}*100/$numberOfFlipsDoublet" | bc ` "
-echo "TH percentage:`echo "scale=2; ${flipCoinDictionary[TH]}*100/$numberOfFlipsDoublet" | bc ` "
+echo "HH percentage:`echo "scale=4; ${flipCoinDictionary[HH]}*100/$numberOfFlipsDoublet" | bc -1` "
+echo "TT percentage:`echo "scale=4; ${flipCoinDictionary[TT]}*100/$numberOfFlipsDoublet" | bc -1` "
+echo "HT percentage:`echo "scale=4; ${flipCoinDictionary[HT]}*100/$numberOfFlipsDoublet" | bc -1` "
+echo "TH percentage:`echo "scale=4; ${flipCoinDictionary[TH]}*100/$numberOfFlipsDoublet" | bc -1` "
 }
 
 function getTripletCombination(){
@@ -79,65 +84,64 @@ function getTripletCombination(){
 read -p "Enter the number of flips you want to perform for triplet:"numberOfFlipsTriplet
 for((flipCount=1;flipCount<=$numberOfFlipsTriplet;flipCount++))
 do
-	case $((RANDOM%8+1)) in
+	case $((RANDOM%8)) in
 	1)
-	flipCoinDictionary[HHH]=$((${flipCoinDictionary[HHH]}+1))
-	echo -ne "HHH"
-	;;
+		flipCoinDictionary[HHH]=$((${flipCoinDictionary[HHH]}+1))
+		;;
 	2)
-   	flipCoinDictionary[TTT]=$((${flipCoinDictionary[TTT]}+1))
-   	echo -ne "TTT"
-   	;;
+   		flipCoinDictionary[TTT]=$((${flipCoinDictionary[TTT]}+1))
+   		;;
 	3)
-   	flipCoinDictionary[HHT]=$((${flipCoinDictionary[HHT]}+1))
-   	echo -ne "HHT"
-   	;;
+   		flipCoinDictionary[HHT]=$((${flipCoinDictionary[HHT]}+1))
+   		;;
 	4)
-   	flipCoinDictionary[TTH]=$((${flipCoinDictionary[TTH]}+1))
-   	echo -ne "TTH"
-   	;;
+   		flipCoinDictionary[TTH]=$((${flipCoinDictionary[TTH]}+1))
+   		;;
 	5)
-   	flipCoinDictionary[THT]=$((${flipCoinDictionary[THT]}+1))
-   	echo -ne "THT"
+   		flipCoinDictionary[THT]=$((${flipCoinDictionary[THT]}+1))
 		;;
 	6)
-   	flipCoinDictionary[HTH]=$((${flipCoinDictionary[HTH]}+1))
-   	echo -ne "HTH"
+   		flipCoinDictionary[HTH]=$((${flipCoinDictionary[HTH]}+1))  
 		;;
 	7)
-   	flipCoinDictionary[THH]=$((${flipCoinDictionary[THH]}+1))
-   	echo -ne "THH"
+   		flipCoinDictionary[THH]=$((${flipCoinDictionary[THH]}+1))
 		;;
 	8)
-   	flipCoinDictionary[HTT]=$((${flipCoinDictionary[HTT]}+1))
-   	echo -ne "HTT"
+   		flipCoinDictionary[HTT]=$((${flipCoinDictionary[HTT]}+1))
 		;;
 	esac
 done
+	#Print the dictionary for triplet combination
+	
+	for key in "${!flipCoinDictionary[@]}"
+	do
+		echo "${key} : ${flipCoinDictionary[$key]}"
+	done
+	
 
 echo ""
-echo "HHH percentage: `echo "scale=2; ${flipCoinDictionary[HHH]}*100/$numberOfFlipsTriplet" | bc ` "
-echo "TTT percentage: `echo "scale=2; ${flipCoinDictionary[TTT]}*100/$numberOfFlipsTriplet" | bc ` "
-echo "HTH percentage: `echo "scale=2; ${flipCoinDictionary[HTH]}*100/$numberOfFlipsTriplet" | bc ` "
-echo "THT percentage: `echo "scale=2; ${flipCoinDictionary[THT]}*100/$numberOfFlipsTriplet" | bc ` "
-echo "HHT percentage: `echo "scale=2; ${flipCoinDictionary[HHT]}*100/$numberOfFlipsTriplet" | bc ` "
-echo "TTH percentage: `echo "scale=2; ${flipCoinDictionary[TTH]}*100/$numberOfFlipsTriplet" | bc ` "
-echo "THH percentage: `echo "scale=2; ${flipCoinDictionary[THH]}*100/$numberOfFlipsTriplet" | bc ` "
-echo "HTT percentage: `echo "scale=2; ${flipCoinDictionary[HTT]}*100/$numberOfFlipsTriplet" | bc ` "
+echo "HHH percentage: `echo "scale=4; ${flipCoinDictionary[HHH]}*100/$numberOfFlipsTriplet" | bc -1` "
+echo "TTT percentage: `echo "scale=4; ${flipCoinDictionary[TTT]}*100/$numberOfFlipsTriplet" | bc -1` "
+echo "HTH percentage: `echo "scale=4; ${flipCoinDictionary[HTH]}*100/$numberOfFlipsTriplet" | bc -1` "
+echo "THT percentage: `echo "scale=4; ${flipCoinDictionary[THT]}*100/$numberOfFlipsTriplet" | bc -1` "
+echo "HHT percentage: `echo "scale=4; ${flipCoinDictionary[HHT]}*100/$numberOfFlipsTriplet" | bc -1` "
+echo "TTH percentage: `echo "scale=4; ${flipCoinDictionary[TTH]}*100/$numberOfFlipsTriplet" | bc -1` "
+echo "THH percentage: `echo "scale=4; ${flipCoinDictionary[THH]}*100/$numberOfFlipsTriplet" | bc -1` "
+echo "HTT percentage: `echo "scale=4; ${flipCoinDictionary[HTT]}*100/$numberOfFlipsTriplet" | bc -1` "
 }
 function sortingTheValues() {
 
 echo "Count of each combination is: "
 for value in "${!flipCoinDictionary[@]}"
 do
-	echo -ne "$value:${flipCoinDictionary["$value"]}"
-done
+	echo "$value:${flipCoinDictionary["$value"]}"
+done | sort -rn -k3
 echo""
 echo "Winning combination is:"
 for value in "${!flipCoinDictionary[@]}"
 do
 	echo "$value: ${flipCoinDictionary["$value"]}"
-done
+done | sort -rn -k3 | head -1
 }
 while(($choice<4))
 do
@@ -145,7 +149,7 @@ do
 	echo "1.Flip coin for singlet combination"
 	echo "2.Flip coin for doublet combination"
 	echo "3.Flip coin for triplet combination"
-	read -p "Enter your choice:"choice
+	read -p "Enter your choice:" choice
 	case $choice in
 		1)
 			getSingletCombination ;;
